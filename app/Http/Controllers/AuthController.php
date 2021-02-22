a37485ab0e5c6e412859936200aa613a827c1df3
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2021-02-22 16:20:04 +0700

    Add authentication route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    //
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('members', 'UserController@index');
 Route::get('members/{name}', 'UserController@show');
+
+Route::post('login', 'AuthController@login');
